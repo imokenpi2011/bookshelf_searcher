21d31ddb2e898557766f89c70b1557031444cbf5
anthem-landing18 <imokenpi2011@gmail.com>
2022-01-03 03:31:08 +0900

    select部分のレコード取得処理を実装

--- src/main/kotlin/com/library/bookshelf_searcher/domain/repository/BookshelfRepository.kt
+++ src/main/kotlin/com/library/bookshelf_searcher/domain/repository/BookshelfRepository.kt
@@ -1,22 +1,29 @@
 package com.library.bookshelf_searcher.domain.repository
 
 import com.library.bookshelf_searcher.domain.model.Book
+import com.library.bookshelf_searcher.domain.repository.db.Tables.BOOKSHELF
+import com.library.bookshelf_searcher.domain.repository.db.tables.Bookshelf
+import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
+
 
 /**
  * Bookshelfリポジトリクラス.
  */
 @Repository
 @Transactional
-class BookshelfRepository {
+class BookshelfRepository(private val dsl: DSLContext) {
 
     /**
      * 全件取得する.
      *
      * ＠return books 書籍情報の配列
      * */
-    fun findAll() {}
+    fun findAll(): List<Bookshelf> = dsl.select()
+        .from(BOOKSHELF)
+        .fetch()
+        .into(Bookshelf::class.java)
 
     /**
      * IDに応じた書籍を取得する.
@@ -24,7 +31,13 @@ class BookshelfRepository {
      * @args uuid UUID
      * @return book 書籍情報
      */
-    fun findByUuid(uuid: String) {}
+    fun findByUuid(uuid: String): Bookshelf? = dsl.select()
+        .from(BOOKSHELF)
+        .where(BOOKSHELF.UUID.eq(uuid))
+        .limit(1)
+        .fetch()
+        .into(Bookshelf::class.java)
+        .firstOrNull()
 
     /**
      * 書籍情報を新規作成する.
