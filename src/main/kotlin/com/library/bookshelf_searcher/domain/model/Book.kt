a4cd64b387e2af886c571fd844c893e8edfcaf77
anthem-landing18 <imokenpi2011@gmail.com>
2022-01-03 03:44:09 +0900

    endlineを追加 / インデントを修正

--- src/main/kotlin/com/library/bookshelf_searcher/domain/model/Book.kt
+++ src/main/kotlin/com/library/bookshelf_searcher/domain/model/Book.kt
@@ -3,17 +3,17 @@ package com.library.bookshelf_searcher.domain.model
 /**
  * Bookクラス.
  * */
-data class Book (
+data class Book(
     val id: Int?,   // ID
     val uuid: String?,  // UUID
     val bookName: String?,  // タイトル
     val authorName: String?  // 著者名
-    )
+)
 
 /**
  * Bookレスポンスクラス.
  */
-data class BookResponse (
+data class BookResponse(
     val status: Int?,   // ステータス
     val message: String?,  // メッセージ
     val book: Book?  // Bookクラス
