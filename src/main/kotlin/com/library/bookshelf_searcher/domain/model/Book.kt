a081e12b64a20a62c0698b7daba6a7961cfcb2a5
anthem-landing18 <imokenpi2011@gmail.com>
2022-01-04 01:23:08 +0900

    Bookクラスのnullを許容しない様変更

--- src/main/kotlin/com/library/bookshelf_searcher/domain/model/Book.kt
+++ src/main/kotlin/com/library/bookshelf_searcher/domain/model/Book.kt
@@ -4,10 +4,10 @@ package com.library.bookshelf_searcher.domain.model
  * Bookクラス.
  * */
 data class Book(
-    val id: Int?,   // ID
-    val uuid: String?,  // UUID
-    val bookName: String?,  // タイトル
-    val authorName: String?  // 著者名
+    val id: Int,   // ID
+    val uuid: String,  // UUID
+    val bookName: String,  // タイトル
+    val authorName: String  // 著者名
 )
 
 /**
