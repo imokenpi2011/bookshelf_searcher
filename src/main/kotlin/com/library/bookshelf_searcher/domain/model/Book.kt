51ac62218031135c99c3364e6ca9e2378ce5d133
anthem-landing18 <imokenpi2011@gmail.com>
2022-01-04 14:54:06 +0900

    不使用の項目を削除

--- src/main/kotlin/com/library/bookshelf_searcher/domain/model/Book.kt
+++ src/main/kotlin/com/library/bookshelf_searcher/domain/model/Book.kt
@@ -4,7 +4,6 @@ package com.library.bookshelf_searcher.domain.model
  * Bookクラス.
  * */
 data class Book(
-    val id: Int,   // ID
     val uuid: String,  // UUID
     val bookName: String,  // タイトル
     val authorName: String  // 著者名
