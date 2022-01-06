3917dfcd0ba12dc730f8968946cb6acdae2bf2e8
anthem-landing18 <imokenpi2011@gmail.com>
2022-01-07 04:39:51 +0900

    処理のコメントを追加 / エラーハンドリングを追加

--- src/main/kotlin/com/library/bookshelf_searcher/app/controller/BookshelfController.kt
+++ src/main/kotlin/com/library/bookshelf_searcher/app/controller/BookshelfController.kt
@@ -21,6 +21,7 @@ class BookshelfController(private val bookshelfService: BookshelfService) {
     /** 検索画面. */
     @GetMapping("/book/search")
     fun getSearchPage(@ModelAttribute formBook: FormBook, model: Model): String {
+        // 検索画面に遷移
         model.addAttribute("formBook", formBook)
         return "bookshelf/index"
     }
@@ -29,15 +30,22 @@ class BookshelfController(private val bookshelfService: BookshelfService) {
     @PostMapping("/book/search")
     fun postSearchPage(@ModelAttribute formBook: FormBook, model: Model): String {
         val authorName: String = formBook.authorName.toString()
+
+        // 検索処理を実行
         val books: List<Book> = bookshelfService.findByAuthor(authorName)
         model.addAttribute("books", books)
+
+        // 検索画面に遷移
         return "bookshelf/index"
     }
 
     /** 書籍一覧表示画面. */
     @GetMapping("/book/list")
     fun getBookList(model: Model): String {
+        // 書籍一覧を取得
         val books: List<Book> = bookshelfService.findAll()
+        
+        // 一覧画面に遷移
         model.addAttribute("books", books)
         return "bookshelf/bookList"
     }
@@ -45,6 +53,7 @@ class BookshelfController(private val bookshelfService: BookshelfService) {
     /** 書籍登録画面. */
     @GetMapping("/book/new")
     fun getBookNew(@ModelAttribute formBook: FormBook, model: Model): String {
+        // 登録画面に遷移
         model.addAttribute("formBook", formBook)
         return "bookshelf/bookNew"
     }
@@ -52,19 +61,29 @@ class BookshelfController(private val bookshelfService: BookshelfService) {
     /** 書籍登録処理. */
     @PostMapping("/book/new")
     fun postBookNew(@ModelAttribute @Validated formBook: FormBook, bindingResult: BindingResult, model: Model): String {
+        // バリデーションエラーを確認
         if (bindingResult.hasErrors()) {
             println(bindingResult.allErrors.map { it.defaultMessage }.joinToString(separator = "\n"))
             model.addAttribute("formBook", formBook)
             return "bookshelf/bookNew"
         }
-        bookshelfService.save(formBook)
+
+        // 登録処理
+        if (bookshelfService.save(formBook) == 0) {
+            // 登録件数が0だった場合は登録画面に遷移
+            model.addAttribute("formBook", formBook)
+            return "bookshelf/bookNew"
+        }
         return "redirect:/book/list"
     }
 
     /** 書籍更新画面. */
     @GetMapping("/book/update/{uuid}")
     fun getBookUpdate(@PathVariable uuid: String, model: Model): String {
-        val book: Book? = bookshelfService.findByUuid(uuid)
+        // 書籍情報を取得. 失敗した場合は一覧画面に遷移.
+        val book: Book = bookshelfService.findByUuid(uuid) ?: return getBookList(model)
+
+        // 更新画面に遷移
         model.addAttribute("book", book)
         return "bookshelf/bookUpdate"
     }
@@ -72,13 +91,31 @@ class BookshelfController(private val bookshelfService: BookshelfService) {
     /** 書籍更新処理. */
     @PostMapping("/book/update")
     fun postBookUpdate(@ModelAttribute @Validated book: Book, bindingResult: BindingResult, model: Model): String {
+        // バリデーションエラーを確認
+        if (bindingResult.hasErrors()) {
+            println(bindingResult.allErrors.map { it.defaultMessage }.joinToString(separator = "\n"))
+            model.addAttribute("book", book)
+            return "bookshelf/bookUpdate"
+        }
+
+        // 更新処理
+        if (bookshelfService.update(book) == 0) {
+            // 更新件数が0だった場合は更新画面に遷移
+            model.addAttribute("book", book)
+            return "bookshelf/bookUpdate"
+        }
+
+        // 更新が成功したら一覧画面に遷移
         return getBookList(model)
     }
 
     /** 書籍削除処理 */
     @PostMapping("/book/delete/{uuid}")
     fun postBookDelete(@PathVariable uuid: String, model: Model): String {
+        // 削除処理
         bookshelfService.delete(uuid)
+
+        // 削除が成功したら一覧画面に遷移
         return getBookList(model)
     }
 
